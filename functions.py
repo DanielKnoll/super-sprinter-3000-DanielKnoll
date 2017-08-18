f89e462b90e122abdc15164dd12b513d5ae63dfd
DanielKnoll <knoll.dani@gmail.com>
2017-08-18 22:09:00 +0200

    Renamed story variables to all_story.

--- functions.py
+++ functions.py
@@ -5,13 +5,13 @@ def get_filename():
 def get_story_from_file(file_name):
     with open(file_name, "r") as file:
         lines = file.readlines()
-    story = [element.replace("\n", "").split(";") for element in lines]
-    return story
+    all_story = [element.replace("\n", "").split(";") for element in lines]
+    return all_story
 
 
-def write_story_to_file(file_name, story):
+def write_story_to_file(file_name, all_story):
     with open(file_name, "w") as file:
-        for record in story:
+        for record in all_story:
             row = ';'.join(record)
             file.write(row + "\n")
 
@@ -19,16 +19,16 @@ def write_story_to_file(file_name, story):
 def get_story():
     file_name = get_filename()
     try:
-        story = get_story_from_file(file_name)
+        all_story = get_story_from_file(file_name)
     except FileNotFoundError:
         return
-    return story
+    return all_story
 
 
-def write_story(story):
+def write_story(all_story):
     file_name = get_filename()
     try:
-        write_story_to_file(file_name, story)
+        write_story_to_file(file_name, all_story)
     except FileNotFoundError:
         return
 
@@ -43,25 +43,25 @@ def make_list_from_form(dictionary):
 
 def save_new_story(new_story):
     new_story = make_list_from_form(new_story)
-    story = get_story()
-    story.append(new_story)
-    new_id = str(len(story))
-    story[-1].insert(0, new_id)
-    write_story(story)
+    all_story = get_story()
+    all_story.append(new_story)
+    new_id = str(len(all_story))
+    all_story[-1].insert(0, new_id)
+    write_story(all_story)
 
 
 def save_edited_story(id_, edited_story):
     edited_story = make_list_from_form(edited_story)
     edited_story.insert(0, str(id_+1))
-    story = get_story()
-    story[id_] = edited_story
-    write_story(story)
+    all_story = get_story()
+    all_story[id_] = edited_story
+    write_story(all_story)
 
 
 def delete_story(id_):
-    story = get_story()
-    for i, line in enumerate(story):
+    all_story = get_story()
+    for i, line in enumerate(all_story):
         if line[0] == str(id_):
-            story.pop(i)
+            all_story.pop(i)
             break
-    write_story(story)
+    write_story(all_story)

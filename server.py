39f8d4007dd304ac7e9ff209754c8a6f5393bce4
DanielKnoll <knoll.dani@gmail.com>
2017-08-19 06:35:53 +0200

    Fixed: Submit buttons in form returned error 400.

--- server.py
+++ server.py
@@ -6,21 +6,26 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/list")
 def route_list():
-    story = functions.get_story()
-    return render_template("list.html", story=story)
+    all_story = functions.get_story()
+    return render_template("list.html", all_story=all_story)
 
 
-@app.route("/story", methods=["GET", "POST"])
-@app.route("/story/<int:id_>", methods=["GET", "POST"])
+@app.route("/story", methods=["GET"])
+@app.route("/story/<int:id_>", methods=["GET"])
 def route_edit(id_=None):
     if id_:
-        story = functions.get_story()
-        title = story[id_][1]
-        story = story[id_][2]
-        criteria = story[id_][3]
-        bis_value = story[id_][4]
-        estimation = story[id_][5]
-        status = story[id_][6]
+        all_story = functions.get_story()
+        try:
+            index = functions.find_line_index_by_id(id_, all_story)
+        except ValueError:
+            return
+        one_story = all_story[index]
+        title = one_story[1]
+        story = one_story[2]
+        criteria = one_story[3]
+        bis_value = one_story[4]
+        estimation = one_story[5]
+        status = one_story[6]
         html_code = render_template("form.html",
                                     id_=id_,
                                     title=title,
@@ -35,18 +40,22 @@ def route_edit(id_=None):
 
 
 @app.route("/save-story", methods=["POST"])
-@app.route("/edit-story", methods=["POST"])
-@app.route("/delete-story/<int:id_>", methods=["GET"])
-def route_save(id_=None):
-    if request.path == "/save-story":
-        new_story = request.form
-        print(new_story)
-        functions.save_new_story(new_story)
-    if request.path == "/edit-story":
-        edited_story = request.form
-        functions.save_edited_story(id_, edited_story)
-    if request.path == "/delete-story/" + str(id_):
-        functions.delete_story(id_)
+def save_new():
+    new_story = request.form
+    functions.save_new_story(new_story)
+    return redirect("/")
+
+
+@app.route("/story/<int:id_>", methods=["POST"])
+def save_edited(id_):
+    edited_story = request.form
+    functions.save_edited_story(id_, edited_story)
+    return redirect("/")
+
+
+@app.route("/delete-story/<int:id_>", methods=["POST"])
+def del_story(id_):
+    functions.delete_story(id_)
     return redirect("/")
 
 
